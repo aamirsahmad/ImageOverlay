038c1d3d04a37189795238e371fde051e2dfe77c
joshchakkal <joshuachakkal@gmail.com>
2017-03-27 12:33:12 -0400

    Update overlays.js

--- overlays.js
+++ overlays.js
@@ -1,16 +1,13 @@
 var overlayImage = document.getElementById("overlayImage");
 
 //Add filenames for overlays here
-var barOverlay = 'bar-overlay.png';
 var cloudOverlay = 'cloud-overlay.png';
 var filter1Overlay = 'filter1.png';
 var filter2Overlay = 'filter2.png';
-//var filter3Overlay = 'filter3.png'; //Duplicate
-
 
 //Add overlays to array
 
-var overlays = [filter1Overlay, filter2Overlay, barOverlay, cloudOverlay ];
+var overlays = [filter1Overlay, filter2Overlay, cloudOverlay ];
 
 var position = 0;
 
