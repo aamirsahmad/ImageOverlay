f5586d83c72f164204c2e701a77526d7cc3a44fa
bradbooth <brad_booth@live.com>
2017-03-26 20:28:40 -0400

    Rotate Scaling

--- rotate.js
+++ rotate.js
@@ -1,9 +1,7 @@
 
 
-
-
-    function rotateImage(){
-      var mainImage = document.getElementById("mainImageContainer");
+function rotateImage(){
+    var mainImage = document.getElementById("mainImageContainer");
 
 
     //Get image url so we can compute sizing ratios
@@ -26,8 +24,8 @@
 
       console.log("Rotating");
       //Create the canvas for changing orientation
-      var oCanvas = document.getElementById("testCanvas"); //Use this for testing locally
-      //var oCanvas = document.createElement("canvas");
+      //var oCanvas = document.getElementById("testCanvas"); //Use this for testing locally
+      var oCanvas = document.createElement("canvas");
 
       oCanvas.width = imageHeight;
       oCanvas.height = imageWidth;
@@ -47,6 +45,44 @@
     var imageUrl = 'url(' + savedImage.src  + ')';//Convert DataURL to url
     mainImage.style.backgroundImage = imageUrl;//Set background
 
+    //*****SCALING - Should be consolidated to a function! *******//
+    var scaleHorizontal;
+    var scaleVertical;
 
+    //Determine wether the width or height is the scaling factor (ie a landscape photo would scale so the height fits)
+    if(imageHeight < imageWidth){
+      scaleHorizontal = false;
+      scaleVertical = true;
+    }else{
+      scaleHorizontal = true;
+      scaleVertical = false;
+    }
+
+    //Scale image so it always fills the screen
+    if(scaleHorizontal){
+      scale = 1080/imageWidth;
+      slider.value = 100;
+      imagePositionX = 0;
+      imagePositionY = -1*((imageHeight*scale)-1080)/2;
+    }else if(scaleVertical){
+      //console.log("Scaling Vertically");
+      scale = 1080/imageHeight;
+      slider.value = 100*((imageWidth*scale)/1080);
+
+      imagePositionX = -1*((imageWidth*scale)-1080)/2;
+      imagePositionY = 0;
+    }
+
+
+   //console.log(imageWidth + 'X' + imageHeight + ' Slider: ' + slider.value);
+    
+    //Center the image vertically and horizontally
+    horizontalScaleRatio = 1080/imageWidth;
+
+    updateSlider();
+    //Update position of background
+    image.style.backgroundPositionX = imagePositionX + 'px';
+    image.style.backgroundPositionY = imagePositionY + 'px';
 
     }
+
--- upload.js
+++ upload.js
@@ -1,5 +1,5 @@
 var OrientationNumber;
- document.getElementById("versionNumber").innerHTML = ("Version 2"); 
+
 function previewFile() {
   //Taken from:
 	//https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
@@ -13,11 +13,58 @@ function previewFile() {
   //Load the image
   reader.addEventListener("load", function () {
 
+    
+
     //Load image into background
-  	var imageUrl = 'url(' + reader.result + ')';//Convert DataURL to url
+  	 var imageUrl = 'url(' + reader.result + ')';//Convert DataURL to url
+    mainImage.style.backgroundImage = imageUrl;//Set background
+
+    //Get image url so we can compute sizing ratios
+    style = window.getComputedStyle(mainImage, false); //Get the updated style page
+    src = style.getPropertyValue("background-image").slice(4,-1).replace(/"/g, ""); //Get background url and rid excess characters
+
+    //Get the original size of the image
+    savedImage = new Image();
+    savedImage.src = src;
+
+
+    imageWidth = savedImage.width;
+    imageHeight = savedImage.height;
+
+    //*ORIENTATION FIXING*//
+    //Should be consolidated into seperate function
+
+    console.log("Orientation Number:" + OrientationNumber);
+
+    //ROTATE 90 DEGREES CW
+    if(OrientationNumber == 6){ 
+      console.log("Rotating");
+      //Create the canvas for changing orientation
+      var oCanvas = document.getElementById("testCanvas"); //Use this for testing locally
+      //var oCanvas = document.createElement("canvas");
+
+      oCanvas.width = imageHeight;
+      oCanvas.height = imageWidth;
+
+      var oContext = oCanvas.getContext("2d");
+     
+      //oContext.rotate(0.5 * Math.PI); //Rotate Canvas 90 Degrees
+      //oContext.translate(0, -oCanvas.width); //Move back into position
+      oContext.drawImage(savedImage, 0 ,0); //Update Canvas
+
+      //Overwrite save image with the rotated image
+      savedImage.src = oCanvas.toDataURL();
+      //Update height and width variables
+      imageWidth = savedImage.width;
+      imageHeight = savedImage.height;
+
+    }
+
+    var imageUrl = 'url(' + savedImage.src  + ')';//Convert DataURL to url
     mainImage.style.backgroundImage = imageUrl;//Set background
 
 
+    //*****SCALING - Should be consolidated to a function! *******//
     var scaleHorizontal;
     var scaleVertical;
 
@@ -104,4 +151,9 @@ function getOrientation(file, callback) {
     return callback(-1);
   };
   reader.readAsArrayBuffer(file);
+}
+
+
+function scale(){
+ 
 }
